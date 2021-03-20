51c7644d2b4f23e91cc4e7cacd0a5af7647f1c79
stephp23 <stephp23@gmail.com>
2021-03-19 20:53:11 -0400

    finished

--- src/componentsViews/DeathCount.js
+++ src/componentsViews/DeathCount.js
@@ -33,18 +33,7 @@ const DeathCount = () => {
           </div>
           )
       })}
-      {/* <button onClick={fetchRandomCharacters}>Surprise Me</button> */}
-      {/* <h2>{randomCharacters.name}</h2> */}
-      {/* <h3>Alias: "{randomCharacters.name}"</h3> */}
-      {/* <img src={randomCharacters.img} alt="character-picture" /> */}
-      {/* <div className="character-information"> */}
-        {/* <h3>Occupation: {randomCharacters.occupation}</h3>
-        <h3>Show: {randomCharacters.category}</h3>
-        <h3>Survival Status: {randomCharacters.status}</h3>
-      </div> */}
-      {/* <div>
-      <button onClick={fetchRandomCharacters}>Lets Try Again</button>
-      </div> */}
+     
     </div>
   )
 }
